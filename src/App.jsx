01170f69259365c290accb4ee305178f05a1aabb
MarwenLabidi <labidimarwen6@gmail.com>
2022-08-14 09:04:14 +0100

    ➕ Adding  : i create the component and the structure of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/header/Header";
+import Body from "./components/body/Body";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    return (
+        <>
+            <div>App</div>
+            <Header />
+            <Body />
+        </>
+    );
+};
 
-export default App
-
+export default App;
---
+++ src/components/body/Body.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "./main/Main";
+
+const Body = () => {
+    return (
+        <>
+            <div>body</div>
+            <Main />
+        </>
+    );
+};
+
+export default Body;
