6a1a4a5532cd3ae4c5170d1407fffa5b9d4de93f
MarwenLabidi <labidimarwen6@gmail.com>
2022-08-21 16:22:26 +0100

    🚧 Work in progress : im working on the dark mode feature

--- src/setup/styled_components/theme.js
+++ src/setup/styled_components/theme.js
@@ -1,12 +1,17 @@
+//TODO? get style varriale from css global style
+let root = document.querySelector(":root");
+let bodyLightTheme = getComputedStyle(root).getPropertyValue("--clr_Very_Light_Gray");
+let bodyDarkTheme = getComputedStyle(root).getPropertyValue("--clr_Very_Dark_Blue");
+
 export const lightTheme = {
-        body: "#E2E2E2",
+        body: bodyLightTheme,
         text: "#363537",
         toggleBorder: "#FFF",
         gradient: "linear-gradient(#39598A, #79D7ED)",
 };
 
 export const darkTheme = {
-        body: "#363537",
+        body: bodyDarkTheme,
         text: "#FAFAFA",
         toggleBorder: "#6B8096",
         gradient: "linear-gradient(#091236, #1E215D)",
